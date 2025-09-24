01567d5c4639c81db317b3016653549cb5c6a722
Calum Cullen <calum.cullen66@gmail.com>
2025-09-24 10:41:28 +0100

    Type aliases demonstration

--- src/myTypes.d.ts
+++ src/myTypes.d.ts
@@ -26,3 +26,17 @@ export interface EmailContact {
     email: string
 }
 
+export type Department = "Engineering" | "Finance" | "HR";
+export interface ColleagueV2 {
+  name: string;
+  department: Department;
+  contact: {
+    email: string;
+    extension: number;
+    slack?: string;
+  };
+}
+
+export type Buddy = Friend | ColleagueV2;
+export type Administrator = Buddy | string | undefined
+
