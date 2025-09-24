34e0dda0cb58879a8a35cdbe860d988c3eac1929
Calum Cullen <calum.cullen66@gmail.com>
2025-09-24 11:54:38 +0100

    define types using Utility types

--- src/myTypes.d.ts
+++ src/myTypes.d.ts
@@ -40,3 +40,18 @@ export interface ColleagueV2 {
 export type Buddy = Friend | ColleagueV2;
 export type Administrator = Buddy | string | undefined
 
+export type BuddyList = {
+  name: string;
+  administrator: Administrator;
+  members: Buddy[];
+};
+
+export type FriendPartial = Partial<Friend>
+// Type for gaining access to an event, e.g. concert.
+export type EventPass = Omit<Colleague, "contact"> & {
+  passCode : number;
+}
+// Immutable person type, based on Friend type.
+export type SecureFriendContact = Readonly<Pick<Friend,"name" | "phone" > >
+
+
